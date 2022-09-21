e736e6495d8a21258102f48fddac9cd0b96046dd
Lynz951 <lindsay.haddix@student.awesomeinc.org>
2022-09-21 19:43:23 +0000

    kinda working

--- JS/main.js
+++ JS/main.js
@@ -1,45 +1,95 @@
 const nextButton = document.getElementById("nextButton");
+const resetButton = document.getElementById("resetButton");
 
 let views= [
     {
       headerText: "I can read your mind",
-      key: 'value',
+      helperText: "null",
     },
     {
       headerText: "Pick a number from 01-99",
+      helperText: "when you have your number click next",
     },
 
-    {headerText: "Add both didgits together to get a new number"},
+    {
+      headerText: "Add both didgits together to get a new number",
+     helperText: "Ex: 14 is 1 + 4 = 5, click next to proceed",
+    },
 
-    {headerText: "Subtract your new number from the original number"},
+    {
+      headerText: "Subtract your new number from the original number",
+      helperText: "Ex: 14 - 5 = 9, click next to proceed",
+    },
 
-    {headerText: "0 - & <br> 1 - @ <br> 2 - $ <br> 3 - B <br> ..."},
+    {
+      headerText: "0 - & <br> 1 - @ <br> 2 - $ <br> 3 - B <br> ...",
+      helperText: "Find your new number. Note the symbol beside the number",
+    },
 
-    {headerText: "&"},
+    {
+      headerText: "&",
+      helperText: "Your symbol is: &",
+    },
     ]
 
 const headerText = document.getElementById("headerText");
-const helpText
+const helperText = document.getElementById("helperText");
 
 nextButton.addEventListener("click", () => {
 
   if (headerText.textContent === views[0].headerText) {
     headerText.innerHTML = views[1].headerText;
-    ()
+    helperText.innerHTML = views[1].helperText;
   } 
+    
   else if (headerText.textContent === views[1].headerText) {
     headerText.innerHTML = views[2].headerText;
-    ()
+    helperText.textContent = views[2].helperText;
   }
+    
   else if (headerText.textContent === views[2].headerText) {
     headerText.innerHTML = views[3].headerText;
+    helperText.textContent = views[3].helperText;
   }
+    
   else if (headerText.textContent === views[3].headerText) {
     headerText.innerHTML = views[4].headerText;
+    helperText.textContent = views[4].helperText;
   }
+   
   else if (headerText.textContent === views[4].headerText) {
     headerText.innerHTML = views[5].headerText;
+    helperText.textContent = views[5].helperText;
   }
 });
 
 
+// resetButton.addEventListener("click", () => {
+//   headerText.innerHTML = views[0].headerText;
+//   helperText.innerHTML = views[0].helperText;
+// });
+
+
+// Other way
+
+// var view = 0;
+// const headerText = document.getElementById("headerText");
+// const helperText = document.getElementById("helperText");
+
+// function whatView () {
+//   console.log(views[view]);
+// }
+
+// function nextView (e){
+// nextButton.addEventListener("click", () => {
+//   view++;
+// }).
+// whatView();
+// }
+
+// function reset (e){
+//   resetButton.addEventListener("click", () => {
+//   view = 0;
+// }).
+//   whatView();
+// }
