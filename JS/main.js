fb41894039f24f9fd1d72ddae35df3de207375fa
Lynz951 <lindsay.haddix@student.awesomeinc.org>
2022-09-22 15:19:00 +0000

    blah

--- JS/main.js
+++ JS/main.js
@@ -1,10 +1,11 @@
 const nextButton = document.getElementById("nextButton");
 const resetButton = document.getElementById("resetButton");
 
+
 let views= [
     {
       headerText: "I can read your mind",
-
+      helperText: "",
     },
 
     {
@@ -14,53 +15,56 @@ let views= [
 
     {
       headerText: "Add both didgits together to get a new number",
-
+      helperText: "Ex: 14 is 1 + 4 = 5, click next to proceed",
     },
 
     {
       headerText: "Subtract your new number from the original number",
-
+      helperText: "Ex: 14 - 5 = 9, click next to proceed",
     },
 
     {
       headerText: "0 - & <br> 1 - @ <br> 2 - $ <br> 3 - B <br> ...",
-
+      helperText: "Find your new number. Note the symbol beside the number",
     },
 
     {
-      headerText: "&",
-      helperText: "Your symbol is: &",
+      headerText: "*",
+      helperText: "Your symbol is: *",
     },
     ]
 
 const headerText = document.getElementById("headerText");
 const helperText = document.getElementById("helperText");
+var views = 0;
 
 nextButton.addEventListener("click", () => {
 
-  if (headerText.textContent === views[0].headerText) {
+  if (views === 0) {
     headerText.innerHTML = views[1].headerText;
     helperText.innerHTML = views[1].helperText;
   } 
     
-  else if (headerText.textContent === views[1].headerText) {
+  else if (views === 1) {
     headerText.innerHTML = views[2].headerText;
+    helperText.innerHTML = views[2].helperText;
+  }
 
-    
-  else if (headerText.textContent === views[2].headerText) {
+  else if (views === 2) {
     headerText.innerHTML = views[3].headerText;
-
+    helperText.innerHTML = views[3].helperText;
   }
     
-  else if (headerText.textContent === views[3].headerText) {
+  else if (views === 3) {
     headerText.innerHTML = views[4].headerText;
-
+    helperText.innerHTML = views[4].helperText;
   }
    
-  else if (headerText.textContent === views[4].headerText) {
+  else if (views === 4) {
     headerText.innerHTML = views[5].headerText;
-
+    helperText.innerHTML = views[5].helperText;
   }
+  views +=;
 });
 
 
