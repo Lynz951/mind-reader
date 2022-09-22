56d8ca7a1dda03ef0de93022dbd4c568e682d019
Lynz951 <lindsay.haddix@student.awesomeinc.org>
2022-09-22 19:07:10 +0000

    works

--- JS/main.js
+++ JS/main.js
@@ -23,14 +23,30 @@ let views= [
       helperText: "Ex: 14 - 5 = 9 <br> click next to proceed",
     },
 
-    {
-      headerText: "0 - & <br> 1 - @ <br> 2 - $ <br> 3 - B <br> 4 - % <br> 5 - # <br> 6 - ! <br> 7 - M <br> 8 - ? <br> 9 - * <br> 10 - & <br> 11 - @ <br> 12 - $ <br> 13 - B <br> 14 - % <br> 15 - # <br> 16 - ! <br> 17 - M <br> 18 - * <br> 19 - ? <br>",
-      helperText: "Find your new number. <br> Note the symbol beside the number",
+    
+      // List numbers 01-99
+      // if int % 9 === 0  assign symbol
+      // if int % 9 != assign random symbol
+
+      //function incrementx(){
+        //let x = 0;
+        //for (let index = 0; index < 100; index++) {
+          //let x = index;
+          // x++
+          //console.log(x)
+        //}
+     // }
+     //increment x
+
+      // headerText: "1 - :insertx: /n 2 - :insertx: /n 3 - :insertx: /n 4 - :insertx: /n 5 - :insertx: /n 6 - :insertx: /n 7 - :insertx: /n 8 - :insertx: /n 9 - :inserty:",
+     {
+       headerText: "0 - & <br> 1 - @ <br> 2 - $ <br> 3 - B <br> 4 - % <br> 5 - # <br> 6 - ! <br> 7 - M <br> 8 - ? <br> 9 - :) <br> 10 - & <br> 11 - @ <br> 12 - $ <br> 13 - B <br> 14 - % <br> 15 - # <br> 16 - ! <br> 17 - M <br> 18 - :) <br> 19 - ? <br> 20 - & <br> 21 - @ <br> 22 - $ <br> 23 - B <br> 24 - % <br> 25 - # <br> 26 - ! <br> 27 - :) <br> 28 - M <br> 29 - ? <br> 30 - & <br> 31 - @ <br> 32 - $ <br> 33 - B <br> 34 - % <br> 35 - # <br> 36 - :) <br> 37 - ! <br> 38 - M <br> 39 - ? <br> 40 - & <br> 41 - @ <br> 42 - $ <br> 43 - B <br> 44 - % <br> 45 - :) <br> 46 - ! <br> 47 - M <br> 48 - ? <br> 49 - $ <br> 50 - B <br> 51 - % <br> 52 - # <br> 53 - ! <br> 54 - :) <br> 55 - M <br> 56 - ? <br> 57 - & <br> 58 - @ <br> 59 - $ <br> 60 - B <br> 61 - % <br> 62 - # <br> 63 - :) <br> 64- ! <br> 65 - M <br> 66 - ? <br> 67 - & <br> 68 - @ <br> 69 - $ <br> 70 - B <br> 71 - % <br> 72 - :) <br> 73 - # <br> 74 - ! <br> 75 - M <br> 76 - ? <br> 77 - & <br> 78 - @ <br> 79 - $ <br> 80 - B <br> 81 - :) <br> 82 - % <br> 83 - # <br> 84 - ! <br> 85 - M <br> 86 - ? <br> 87 - & <br> 88 - @ <br> 89 - $ <br> 90 - :) <br> 91 - B <br> 92 - % <br> 93 - # <br> 94 - ! <br> 95 - M <br> 96 - ? <br> 97 - & <br> 98 - @ <br> 99 - :) <br>",
+       helperText: "Find your new number. <br> Note the symbol beside the number",
     },
 
     {
-      headerText: "*",
-      helperText: "Your symbol is: *",
+      headerText: ":)",
+      helperText: "Your symbol is: :)",
     },
     ]
 
@@ -44,6 +60,8 @@ nextButton.addEventListener("click", () => {
   if (view === 0) {
     headerText.innerHTML = views[1].headerText;
     helperText.innerHTML = views[1].helperText;
+    resetButton.classList.remove('d-none');
+    resetButton.classList.add('d-block');
   } 
     
   else if (view === 1) {
@@ -76,3 +94,10 @@ resetButton.addEventListener("click", () => {
     } 
   return view;
 });
+
+// function range(start, end) {
+//   return Array(end - start + 1).fill().map((_, idx) => start + idx)
+// }
+// var result = range(9, 18); // [9, 10, 11, 12, 13, 14, 15, 16, 17, 18]
+// console.log(result);
+
